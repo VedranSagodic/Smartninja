24e2bcf876f03fd979439aa85343e990e92afba0
VedranSagodic <vedran.sagodic@gmail.com>
2020-07-05 07:42:06 +0200

    Lists exercise

---
+++ python1/lists.py
@@ -0,0 +1,25 @@
+#liste
+
+lista_brojeva = [1, 99, 24, 46, 5]
+
+print(lista_brojeva[3])
+print()
+
+lista_brojeva.reverse()
+print(lista_brojeva)
+print()
+
+lista_brojeva.append(100)
+print(lista_brojeva)
+print()
+
+lista_brojeva.sort()
+print(lista_brojeva)
+print()
+
+auti = ["tesla", "audi", "bmw"]
+print(auti)
+
+for item in auti:
+    print(item)
+    
